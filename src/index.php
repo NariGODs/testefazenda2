cae1a96d5fc10bd84c90d166f95241500248fd4e
Felipe Teles <felipe.teles@outlook.com.br>
2022-03-07 22:25:11 -0300

    Nice

--- src/index.php
+++ src/index.php
@@ -1,16 +1,16 @@
 <html>
 
 <!-- Head -->
-<head> 
-	<meta charset="utf-8"> 
+<head>
+	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Chat - PHP</title>
 	<link rel="icon" href="../img/icon.png">
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
 	<link rel="stylesheet" type="text/css" href="../css/estilo.css"/>
-	
-</head> 
+
+</head>
 <!-- Final do Head -->
 	<!-- Inicio do body -->
 	<body>
@@ -28,14 +28,12 @@
 							<h1 class="TextosAparte"> <span id="TituloPrincipal">PHP</span> CHAT</h1>
 							<hr style="border-color: #1c1c1c ; opacity: 25%;">
 							<p class="TextosAparte">
-							Seja bem vindo ao projeto PHP CHAT, este projeto consiste na criação de um site utiliando
-							o php como linguaguem do back-end, no qual será responsável por armazenar as mensagens enviadas
-							para os demais usuários. Este é um sistema simples, visando ajudar aqueles que precisam de um 
-							sistema de chat utilizando a linguagem em PHP. Este é um sistema simplificado e que pode ser
-							melhorado, sinta-se livre para se utilizar dos códigos disponibilizados no GitHub do desenvolvedor deste sistema. Obrigado pela atenção!</p>
+							Seja bem vindo ao projeto nosso chat particular Muahahahhaah
+							Fique a vontade Madame.
+							Obrigado pela atenção!</p>
 
 
-							<span class="TextosAparte">- Pedro Pessoa</span>
+							<span class="TextosAparte">- NariGODs</span>
 							<a href="https://github.com/yzPeedro"><img src="../img/gitlogo.png" class="logos"></a>
 
 					</div>
@@ -47,7 +45,7 @@
 
 						<!-- Divisão que renderiza o chat -->
 							<div class="chat" id="chat">
-									
+
 							</div>
 
 						<!-- Formulário de envios das mensagens -->
@@ -64,7 +62,7 @@
 
 		</div>
 		<!-- Final do container -->
-		
+
 	</body>
 	<!-- Final do body -->
 
@@ -73,5 +71,5 @@
 	<!-- /jQuery -->
 
 	<script type="text/javascript" src="../js/script.js"></script>
-	
+
 </html>
